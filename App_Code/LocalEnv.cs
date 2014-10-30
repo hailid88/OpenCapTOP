063a168fa0a1a95a3e036423269a8af94eec448f
Haili Dong <hailid88@gmail.com>
2014-10-30 14:07:30 -0400

    remove ip
    remove

--- App_Code/LocalEnv.cs
+++ App_Code/LocalEnv.cs
@@ -8,7 +8,7 @@ using System.Web;
 /// </summary>
 public static class LocalEnv
 {
-    
+    //For security purpose, the IP address, id, password are removed. 
     static public string AVLDBConnString = "Server=*;Database=*;User Id=*;Password=*;";
     static public string tirsConnString = "Server=*;Database=*;User Id=*;Password=*;";
     static public string speedConnString = "Server=*;Database=*;User Id=*;Password=*;";
